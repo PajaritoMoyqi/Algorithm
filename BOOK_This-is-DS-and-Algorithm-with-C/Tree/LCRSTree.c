4bbe5f653150e06b2904e04be527403cd4398d16
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-06-04 11:46:56 +0900

    LCRSTree: create, destroy, addChild, print

---
+++ BOOK_This-is-DS-and-Algorithm-with-C/Tree/LCRSTree.c
@@ -0,0 +1,63 @@
+#include "LCRSTree.h"
+
+LCRSNode* LCRS_CreateNode( ElementType NewData )
+{
+    LCRSNode* NewNode = (LCRSNode*)malloc( sizeof( LCRSNode ) );
+    
+    NewNode->Data = NewData;
+    NewNode->LeftChild = NULL;
+    NewNode->RightSibling = NULL;
+
+    return NewNode;
+}
+
+void LCRS_DestroyNode( LCRSNode* Node )
+{
+    free( Node );
+}
+
+void LCRS_DestroyTree( LCRSNode* Root )
+{
+    if ( Root->RightSibling != NULL )
+        LCRS_DestroyNode( Root->RightSibling );
+    
+    if ( Root->LeftChild != NULL )
+        LCRS_DestroyNode( Root->LeftChild );
+
+    Root->LeftChild = NULL;
+    Root->RightSibling = NULL;
+
+    LCRS_DestroyNode( Root );
+}
+
+void LCRS_AddChildNode( LCRSNode* Parent, LCRSNode* Child )
+{
+    if ( Parent->LeftChild == NULL )
+        Parent->LeftChild = Child;
+    else
+    {
+        LCRSNode* TempNode = Parent->LeftChild;
+        while ( TempNode->RightSibling != NULL )
+            TempNode = TempNode->RightSibling;
+
+        TempNode->RightSibling = Child;
+    }
+}
+
+void LCRS_PrintTree( LCRSNode* Node, int Depth )
+{
+    int i = 0;
+    for ( i = 0; i < Depth - 1; i++ )
+        printf( "   " );
+    
+    if ( Depth > 0 )
+        printf( "+--" );
+
+    printf( "%c\n", Node->Data );
+
+    if ( Node->LeftChild != NULL )
+        LCRS_PrintTree( Node->LeftChild, Depth + 1 );
+
+    if ( Node->RightSibling != NULL )
+        LCRS_PrintTree( Node->RightSibling, Depth + 1 );
+}
