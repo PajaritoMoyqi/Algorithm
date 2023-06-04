fd9b26d314ba7efc56f42c3fd2cb8ff19b69431a
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-06-04 12:40:42 +0900

    SimpleBinaryTree: create, destroy, print(in 3 order type)

---
+++ BOOK_This-is-DS-and-Algorithm-with-C/Tree/BinaryTree/BinaryTree.c
@@ -0,0 +1,53 @@
+#include "BinaryTree.h"
+
+SBTNode* SBT_CreateNode( ElementType NewData )
+{
+    SBTNode* NewNode = (SBTNode*)malloc( sizeof( SBTNode ) );
+
+    NewNode->Data = NewData;
+    NewNode->Left = NULL;
+    NewNode->Right = NULL;
+
+    return NewNode;
+}
+
+void SBT_DestroyNode( SBTNode* Node )
+{
+    free( Node );
+}
+
+void SBT_PreorderPrintTree( SBTNode* Node )
+{
+    if ( Node == NULL )
+        return;
+
+    printf( " %c", Node->Data );
+
+    SBT_PreorderPrintTree( Node->Left );
+
+    SBT_PreorderPrintTree( Node->Right );
+}
+
+void SBT_InorderPrintTree( SBTNode* Node )
+{
+    if ( Node == NULL )
+        return;
+
+    SBT_InorderPrintTree( Node->Left );
+
+    printf( " %c", Node->Data );
+
+    SBT_InorderPrintTree( Node->Right );
+}
+
+void SBT_PostorderPrintTree( SBTNode* Node )
+{
+    if ( Node == NULL )
+        return;
+
+    SBT_PostorderPrintTree( Node->Left );
+
+    SBT_PostorderPrintTree( Node->Right );
+
+    printf( " %c", Node->Data );
+}
