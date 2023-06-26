2654a520ec74bdb2f431532a132a4ea1d08d660c
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-06-26 18:11:28 +0900

    SimpleHashTable: create, get, hash, destroy

---
+++ BOOK_This-is-DS-and-Algorithm-with-C/HashTable/SimpleHashTable/SimpleHashTable.c
@@ -0,0 +1,28 @@
+#include "SimpleHashTable.h"
+
+HashTable* SHT_CreateHashTable( int TableSize )
+{
+    HashTable* HT = (HashTable*)malloc( sizeof( HashTable ) );
+    HT->Table = (Node*)malloc( sizeof( Node ) * TableSize );
+    HT->TableSize = TableSize;
+
+    return HT;
+}
+
+void SHT_Get( HashTable* HT, KeyType Key )
+{
+    int Address = SHT_Hash( Key, HT->TableSize );
+
+    return HT->Table[Address].Value;
+}
+
+void SHT_DestroyHashTable( HashTable* HT )
+{
+    free( HT->Table );
+    free( HT );
+}
+
+int SHT_Hash( KeyType Key, int TableSize )
+{
+    return Key % TableSize;
+}
