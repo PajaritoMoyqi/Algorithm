677ca951baf3b4b8b204bd58267806099f8e1bf5
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-06-27 15:46:47 +0900

    MinimumSpanningTree: Kruskal algorithm

--- BOOK_This-is-DS-and-Algorithm-with-C/Graph/MinimumSpanningTree/MST.c
+++ BOOK_This-is-DS-and-Algorithm-with-C/Graph/MinimumSpanningTree/MST.c
@@ -84,3 +84,68 @@ void Prim( Graph* G, Vertex* StartVertex, Graph* MST )
 
     PQ_Destory( PQ );
 }
+
+void Kruskal( Graph* G, Graph* MST )
+{
+    int i;
+    Vertex* CurrentVertex = NULL;
+    Vertex** MSTVertices = (Vertex**)malloc( sizeof( Vertex* ) * G->VertexCount );
+    DisjointSet** VertexSet = (DisjointSet**)malloc( sizeof( DisjointSet* ) * G->VertexCount );
+
+    PriorityQueue* PQ = PQ_Create( 10 );
+
+    i = 0;
+    CurrentVertex = G->Vertices;
+
+    while( CurrentVertex != NULL )
+    {
+        Edge* CurrentEdge;
+
+        VertexSet[i] = DS_MakeSet( CurrentVertex );
+        MSTVertices[i] = CreateVertex( CurrentVertex->Data );
+        AddVertex( MST, MSTVertices[i] );
+
+        CurrentEdge = CurrentVertex->AdjacencyList;
+
+        while( CurrentEdge != NULL )
+        {
+            PQNode NewNode = { CurrentEdge->Weight, CurrentEdge };
+            PQ_Enqueue( PQ, NewNode );
+            
+            CurrentEdge = CurrentEdge->Next;
+        }
+
+        CurrentVertex = CurrentVertex->Next;
+        i++;
+    }
+
+    while( PQ_IsEmpty( PQ ) )
+    {
+        Edge* CurrentEdge;
+        int FromIndex;
+        int ToIndex;
+        PQNode Popped;
+
+        PQ_Dequeue( PQ, &Popped );
+        CurrentEdge = (Edge*)Popped.Data;
+
+        printf( "%c - %c: %d\n", CurrentEdge->From->Data, CurrentEdge->Target->Data, CurrentEdge->Weight );
+
+        FromIndex = CurrentEdge->From->Index;
+        ToIndex = CurrentEdge->Target->Index;
+        
+        if( DS_FindSet( VertexSet[FromIndex] ) != DS_FindSet( VertexSet[ToIndex] ) )
+        {
+            AddEdge( MSTVertices[FromIndex], CreateEdge( MSTVertices[FromIndex], MSTVertices[ToIndex], CurrentEdge->Weight ) );
+            AddEdge( MSTVertices[ToIndex], CreateEdge( MSTVertices[ToIndex], MSTVertices[FromIndex], CurrentEdge->Weight ) );
+
+            DS_UnoinSet( MSTVertices[ToIndex], CreateEdge( MSTVertices[ToIndex], MSTVertices[FromIndex], CurrentEdge->Weight ) );
+        }
+    }
+
+    for( i = 0; i < G->VertexCount; i++ )
+        DS_DestroySet( VertexSet[i] );
+
+    free( VertexSet );
+    free( MSTVertices );
+}
--- BOOK_This-is-DS-and-Algorithm-with-C/Graph/MinimumSpanningTree/MST.h
+++ BOOK_This-is-DS-and-Algorithm-with-C/Graph/MinimumSpanningTree/MST.h
@@ -8,5 +8,6 @@
 #define MAX_WEIGHT 36267
 
 void Prim( Graph* G, Vertex* StartVertex, Graph* MST );
+void Kruskal( Graph* G, Graph* MST );
 
 #endif
