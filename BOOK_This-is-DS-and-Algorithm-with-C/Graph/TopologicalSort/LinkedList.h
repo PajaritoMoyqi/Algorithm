957679cce9eca8ddede7fdfbf1b4f138b20a82f3
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-06-27 14:12:06 +0900

    TopologicalSort: base code(LinkedList, Graph)

---
+++ BOOK_This-is-DS-and-Algorithm-with-C/Graph/TopologicalSort/LinkedList.h
@@ -0,0 +1,30 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+// spacing between # and include for readability
+# include <stdio.h>
+# include <stdlib.h>
+# include "Graph.h"
+
+typedef Vertex* ElementType;
+
+typedef struct tagNode
+{
+    ElementType Data;
+    struct tagNode* NextNode;
+} Node;
+
+// delclaration of functions
+Node* SLL_CreateNode_WrongWay(ElementType NewData);
+Node* SLL_CreateNode(ElementType NewData);
+void SLL_DestroyNode(Node* Node);
+void SLL_AppendNode(Node** Head, Node* NewNode);
+Node* SLL_GetNodeAt(Node* Head, int Location);
+void SLL_RemoveNode(Node** Head, Node* Remove);
+void SLL_InsertAfter(Node* Current, Node* NewNode);
+void SLL_InsertBefore(Node** Head, Node* Current, Node* NewNode);
+void SLL_InsertNewHead(Node** Head, Node* NewHead);
+int SLL_GetNodeCount(Node* Head);
+void SLL_DestroyAllNodes(Node** Head);
+
+#endif
