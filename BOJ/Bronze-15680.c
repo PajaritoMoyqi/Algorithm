de5c70a19973bfb74a421ef465c284b94cb038b9
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-08-06 11:12:07 +0900

    BOJ::15680.c

---
+++ BOJ/Bronze-15680.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main( void )
+{
+    int a;
+    scanf( "%d", &a );
+
+    if ( a )
+        printf( "Leading the Way to the Future" );
+    else
+        printf( "YONSEI" );
+
+    return 0;
+}
