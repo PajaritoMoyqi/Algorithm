2d8486a6d61809df2ba826bc1e3ee413e73152d0
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-08-21 13:25:38 +0900

    BOJ::15652.c(Backtracking)

---
+++ BOJ/Silver-15652.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int max, len;
+char visited[1000];
+char ans[1000];
+
+int main( void )
+{
+    scanf( "%d%d", &max, &len );
+    DFS( max, len, 0, 0 );
+
+    return 0;
+}
+
+void DFS( int maxNum, int maxLen, int depth, int lastNum )
+{
+    if ( depth == maxLen )
+    {
+        int i;
+        for ( i = 0; i < depth; i++ )
+            printf( "%d ", ans[i] );
+        puts( "" );
+    }
+    else
+    {
+        int i;
+        for ( i = 0; i < maxNum; i++ )
+        {
+            if ( lastNum <= i + 1 )
+            {
+                ans[depth] = i + 1;
+
+                DFS( maxNum, maxLen, depth + 1, i + 1 );
+            }
+        }
+    }
+}
