6f3640300192fc3d6ec6722739e542966c67d115
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-08-24 08:05:03 +0900

    BOJ::16195.c(dp)

---
+++ BOJ/Silver-16195.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define MAX 1000
+
+long long ans[MAX+1][MAX+1];
+
+int main( void )
+{
+    ans[1][1] = 1LL;
+    ans[2][1] = 1LL;
+    ans[2][2] = 1LL;
+    ans[3][1] = 1LL;
+    ans[3][2] = 2LL;
+    ans[3][3] = 1LL;
+
+    int i, j;
+    for ( i = 4; i <= MAX; i++ )
+        for ( j = (i-1)/3 + 1; j <= i; j++ )
+            ans[i][j] = (ans[i-1][j-1] + ans[i-2][j-1] + ans[i-3][j-1]) % 1000000009;
+
+    int t;
+    scanf( "%d", &t );
+
+    int n, m;
+    long long sum;
+    while ( t-- )
+    {
+        sum = 0LL;
+        scanf( "%d%d", &n, &m );
+        
+        while ( ans[n][m] )
+            sum = (sum + ans[n][m--]) % 1000000009;
+
+        printf( "%d\n", sum );
+    }
+
+    return 0;
+}
