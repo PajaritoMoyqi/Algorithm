f81cbf5a30fb4f6209dc34c9fefb74b33a704f45
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-08-28 08:26:55 +0900

    BOJ::26766.c(print)

---
+++ BOJ/Bronze-26766.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main( void )
+{
+    int n;
+    scanf( "%d", &n );
+
+    while ( n-- )
+        printf( " @@@   @@@ \n" "@   @ @   @\n" "@    @    @\n" "@         @\n" " @       @ \n" "  @     @  \n" "   @   @   \n" "    @ @    \n" "     @     \n" );
+
+    return 0;
+}
