dad108e885b96feb866f18ac3dd255ace4eb2bf1
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-08-30 11:15:08 +0900

    BOJ::2372.adb(print)

---
+++ BOJ/Bronze-2372.adb
@@ -0,0 +1,13 @@
+with Ada.Text_IO;
+use Ada;
+
+procedure hello is
+begin
+    Text_IO.Put_Line("Animal      Count");
+    Text_IO.Put_Line("-----------------");
+    Text_IO.Put_Line("Chickens      100");
+    Text_IO.Put_Line("Clydesdales     5");
+    Text_IO.Put_Line("Cows           40");
+    Text_IO.Put_Line("Goats          22");
+    Text_IO.Put_Line("Steers          2");
+end hello;
