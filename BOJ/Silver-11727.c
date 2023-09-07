9842c79a89bfbf12bfc678e2537cd4eda755f5f8
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-09-07 09:34:26 +0900

    BOJ::11727.c(dp)

---
+++ BOJ/Silver-11727.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int fib[1000+1];
+
+int main( void )
+{
+    fib[0] = 1;
+    fib[1] = 1;
+
+    int i;
+    for ( i = 2; i <= 1000; i++ )
+        fib[i] = (fib[i-1] + 2*fib[i-2]) % 10007;
+
+    int n;
+    scanf( "%d", &n );
+    
+    printf( "%d", fib[n] );
+
+    return 0;
+}
