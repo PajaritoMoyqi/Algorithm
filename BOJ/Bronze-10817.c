e41f6b1506620cfd3f85666676a0ee88da987acb
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-10-29 08:45:42 +0900

    BOJ::10817.c

---
+++ BOJ/Bronze-10817.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main( void )
+{
+    int a, b, c;
+    scanf( "%d%d%d", &a, &b, &c );
+
+    if ( a >= b && a >= c )
+    {
+        if ( b >= c ) printf( "%d", b );
+        else printf( "%d", c );
+    }
+    else if ( b >= a && b >= c )
+    {
+        if ( a >= c ) printf( "%d", a );
+        else printf( "%d", c );
+    }
+    else
+    {
+        if ( a >= b ) printf( "%d", a );
+        else printf( "%d", b );
+    }
+
+    return 0;
+}
