c3263695346b9f18fff9490cdd6a85669495d107
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-10-31 17:52:48 +0900

    BOJ::1850.c(Euclidean)

---
+++ BOJ/Silver-1850.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+long long getGCD( long long a, long long b );
+
+int main( void )
+{
+    long long a, b, n;
+    scanf( "%lld%lld", &a, &b );
+    n = getGCD( a, b );
+
+    while ( n-- )
+        printf( "1" );
+
+    return 0;
+}
+
+long long getGCD( long long a, long long b )
+{
+    long long big = (a >= b) ? a : b;
+    long long small = (a >= b) ? b : a; 
+    long long mod = big % small;
+
+    if ( mod == 0 )
+        return small;
+    else
+        return getGCD( small, mod );
+}
