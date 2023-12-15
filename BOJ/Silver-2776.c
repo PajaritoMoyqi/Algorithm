4012a1b6a66d9587cacad0b680c717271e92693f
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-12-15 17:29:53 +0900

    BOJ::10815.c(ds, qsort, bsearch)

---
+++ BOJ/Silver-2776.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h> // qsort
+
+int cmp( const void* _elem1, const void* _elem2 );
+
+int main( void )
+{
+    int t, n, m, num, i;
+    scanf( "%d", &t );
+
+    while ( t-- )
+    {
+        scanf( "%d", &n );
+        
+        int NumberSet[n];
+        for ( i = 0; i < n; i++ )
+            scanf( "%d", &NumberSet[i] );
+
+        qsort( NumberSet, n, sizeof( int ), cmp );
+
+        int* find;
+        scanf( "%d", &m );
+        while ( m-- )
+        {
+            scanf( "%d", &num );
+            if ( (find = bsearch( &num, NumberSet, n, sizeof( int ), cmp )) != NULL )
+                printf( "1\n" );
+            else
+                printf( "0\n" );
+        }
+    }
+    
+    return 0;
+}
+
+int cmp( const void* _elem1, const void* _elem2 )
+{
+    int* elem1 = (int*)_elem1;
+    int* elem2 = (int*)_elem2;
+
+    if ( *elem1 > *elem2 )
+        return 1;
+    else if ( *elem1 < *elem2 )
+        return -1;
+    else
+        return 0;
+}
