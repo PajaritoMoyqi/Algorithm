9832da6c09c8f9734cf431a8a2b10f12bfd38530
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-12-18 06:39:59 +0900

    _Retry: String Search - KnuthMorrisPratt

--- BOOK_This-is-DS-and-Algorithm-with-C/_Retry/StringSearch/KnuthMorrisPratt/KMP.h
+++ BOOK_This-is-DS-and-Algorithm-with-C/_Retry/StringSearch/KnuthMorrisPratt/KMP.h
@@ -4,6 +4,7 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-
+void Preprocess( char* Pattern, int patternLength, int* Border );
+int KnuthMorrisPratt( char* Text, int textLength, int startIdx, char* Pattern, int patternLength );
 
 #endif
