6c3524ab4a8f44c36113535d483e8b5cc8d8b41b
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2024-08-11 23:58:41 +0900

    BOJ::15000.py(string)

---
+++ BOJ/Bronze-15000.py
@@ -0,0 +1,18 @@
+### initialization ###
+
+import sys
+
+input = sys.stdin.readline
+MAX_LEN = 10
+
+### declaration ###
+
+def init():
+    string = str( input().rstrip() )
+    print( string.upper() )
+
+    return
+
+### execution ###
+
+init()
