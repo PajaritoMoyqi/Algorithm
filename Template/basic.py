c09bb55e442bbc007564632d3d859cf2a59cb8f6
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2024-08-12 10:15:20 +0900

    Template: C, Python

---
+++ Template/basic.py
@@ -0,0 +1,17 @@
+### initialization ###
+
+import sys
+
+input = sys.stdin.readline
+
+### declaration ###
+
+def init():
+    # n, m = map( int, input().rstrip().split() )
+    # n = int( input() )
+
+    return
+
+### execution ###
+
+init()
