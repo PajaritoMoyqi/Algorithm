26ddf9021d6a203aea68c71bcceb417c18ed33c6
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2024-12-30 11:34:42 +0900

    LC::231.c(bit manipulation, math, recursion)

---
+++ LeetCode/Easy-231-Power_of_Two.c
@@ -0,0 +1,17 @@
+bool isPowerOfTwo( int n )
+{
+    if ( n <= 0 )
+        return false;
+
+    while ( 1 )
+    {
+        if ( n & 1 )
+        {
+            if ( n == 1 )
+                return true;
+            else
+                return false;
+        }
+        n /= 2;
+    }
+}
